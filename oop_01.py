9170af4be7328891c043ca498afa400dcbde3c12
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-15 18:22:20 +0200

    Update oop_01.py

--- oop_01.py
+++ oop_01.py
@@ -3,18 +3,40 @@
 
 class Employee():
     def __init__(self,
+                 employee_id: str,
+                 job_description: str,
                  first_name: str,
                  last_name: str,
                  vat_id: str,
                  email: str,
                  phone: str,
-                 dat_of_birth: str = ''):
+                 date_of_birth: str = ''):
+        self.employee_id = employee_id
+        self.job_description = job_description
         self.first_name = first_name
         self.last_name = last_name
         self.vat_id = vat_id
         self.email = email
         self.phone = phone
-        self.dat_of_birth = dat_of_birth
+        self.date_of_birth = date_of_birth
+
+
+class Customer():
+    def __init__(self,
+                 first_name: str,
+                 last_name: str,
+                 vat_id: str,
+                 email: str,
+                 phone: str,
+                 date_of_birth: str = '',
+                 is_important: bool = False):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.vat_id = vat_id
+        self.email = email
+        self.phone = phone
+        self.date_of_birth = date_of_birth
+        self.is_important = is_important
 
 
 
