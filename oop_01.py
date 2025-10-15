7729c5c40f5e9e547a2ed3a56af38bd8e3438cb9
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-15 18:15:19 +0200

    Create oop_01.py

---
+++ oop_01.py
@@ -0,0 +1,20 @@
+
+
+
+class Employee():
+    def __init__(self,
+                 first_name: str,
+                 last_name: str,
+                 vat_id: str,
+                 email: str,
+                 phone: str,
+                 dat_of_birth: str = ''):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.vat_id = vat_id
+        self.email = email
+        self.phone = phone
+        self.dat_of_birth = dat_of_birth
+
+
+
